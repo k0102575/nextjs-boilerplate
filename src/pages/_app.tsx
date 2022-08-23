15ff1b5f292c92b2c98cc5755b526a806ebe34ba
k0102575 <k0102575@gmail.com>
2022-08-24 03:10:32 +0900

    feat: add react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,23 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import GlobalStyles from '@/components/GlobalStyles';
+import $queryClient from '@/libs/queryClient';
+import { Hydrate, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 import GlobalHead from '../components/GlobalHead';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  // useState
+  const [queryClient] = useState($queryClient);
+
   return (
-    <>
-      <GlobalHead />
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <GlobalHead />
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
   );
 };
 
