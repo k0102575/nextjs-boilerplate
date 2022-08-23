b2a7884779139c1924a52b1aa7ba47533799adf3
k0102575 <k0102575@gmail.com>
2022-08-24 02:04:35 +0900

    feat: add svgr/webpack

--- next.config.js
+++ next.config.js
@@ -3,6 +3,28 @@
 const prod = APP_ENV === 'production';
 
 const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'removeViewBox',
+                  active: false,
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
   swcMinify: true,
   compiler: {
     removeConsole: prod
