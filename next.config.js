8c9ac2d10cadc626c955bf81f0a327c3c204b02d
k0102575 <k0102575@gmail.com>
2022-08-24 02:02:08 +0900

    feat: enable removeConsole

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const prod = APP_ENV === 'production';
+
+const nextConfig = {
+  swcMinify: true,
+  compiler: {
+    removeConsole: prod
+      ? {
+          exclude: ['error'],
+        }
+      : false,
+  },
+};
+
+module.exports = nextConfig;
